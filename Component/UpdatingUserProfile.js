b61f92e7ea906586a18efac708b06725eaafe453
Elizabeth <elizabethli320@yahoo.co.uk>
2023-06-08 20:48:31 +0100

    change19

--- Component/UpdatingUserProfile.js
+++ Component/UpdatingUserProfile.js
@@ -1,87 +1,90 @@
 //UpdatingUserProfile
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from "react-native";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import EmailValidator from 'email-validator';
 import { ScrollView } from 'react-native-web';
+import EmailValidator from 'email-validator';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { globalStyles } from '../globalStyles';
 
 export default class UpdatingUserProfile extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        // Initialize state variables
-        this.state = {           
-            originalData: {},
-            first_name: " ",
-            last_name: " ",
-            email: " ",
-            password: " "
-        }
+    // Initialize state variables
+    this.state = {
+      originalData: {},
+      first_name: " ",
+      last_name: " ",
+      email: " ",
+      password: " "
+    }
+  }
+
+  componentDidMount() {
+    // Set the initial state values based on the received data
+    this.setState({
+      originalData: this.props.route.params.data,
+      first_name: this.props.route.params.data.first_name,
+      last_name: this.props.route.params.data.last_name,
+      email: this.props.route.params.data.email
+    }, () => {
+      console.log(this.state)
+    })
+  }
+
+  // Function to update the user profile and send the updated data to the API
+  UpdatingUserProfile = async () => {
+
+    //Validation:
+    if (!(this.state.email && this.state.password)) {
+      this.setState({ error: "Enter an email and password" })
+      return;
+    } else if (!EmailValidator.validate(this.state.email)) {
+      this.setState({ error: "Enter a valid email" })
+      return;
     }
 
-    componentDidMount() {
-      // Set the initial state values based on the received data
-        this.setState({
-            originalData: this.props.route.params.data,
-            first_name: this.props.route.params.data.first_name,
-            last_name: this.props.route.params.data.last_name,
-            email: this.props.route.params.data.email
-        }, () => {
-            console.log(this.state)
-        })
+    let data = {}
+
+
+    // Check if the email has been changed
+    if (this.state.email !== this.state.originalData.email) {
+      data.email = this.state.email
     }
 
-    // Function to update the user profile and send the updated data to the API
-    UpdatingUserProfile = async () => {
-
-        //Validation:
-        if (!(this.state.email && this.state.password)) {
-            this.setState({ error: "Enter an email and password" })
-            return;
-        } else if (!EmailValidator.validate(this.state.email)) {
-            this.setState({ error: "Enter a valid email" })
-            return;
-        }
-
-        let data = {}
-
-        // Check if the first name has been changed
-        if (this.state.first_name !== this.state.originalData.first_name) {
-            data.first_name = this.state.first_name
-        }
-
-        // Check if the last name has been changed
-        if (this.state.last_name !== this.state.originalData.last_name) {
-            data.last_name = this.state.last_name
-        }
-
-        // Check if the email has been changed
-        if (this.state.email !== this.state.originalData.email) {
-            data.email = this.state.email
-        }
-
-        // Send a PATCH request to the API with the updated data
-        return fetch(`http://localhost:3333/api/1.0.0/user/${this.state.originalData.user_id}`, {
-            method: 'PATCH',
-            headers: {
-                "X-Authorization": await AsyncStorage.getItem("app_session_token"),
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        })
-            .then((response) => {
-                if (response.status === 200) {
-                    console.log("User updated")
-                    this.props.navigation.navigate("AccountProfile");
-                } else {
-                    throw "Something went wrong"
-                }
-            })
-            .catch((err) => {
-                console.log(err)
-            })
+    // Check if the first name has been changed
+    if (this.state.first_name !== this.state.originalData.first_name) {
+      data.first_name = this.state.first_name
     }
+
+    // Check if the last name has been changed
+    if (this.state.last_name !== this.state.originalData.last_name) {
+      data.last_name = this.state.last_name
+    }
+
+
+    // Send a PATCH request to the API with the updated data
+    return fetch(`http://localhost:3333/api/1.0.0/user/${this.state.originalData.user_id}`, {
+      method: 'PATCH',
+      headers: {
+        "X-Authorization": await AsyncStorage.getItem("app_session_token"),
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("User updated")
+          this.props.navigation.navigate("AccountProfile");
+        } else {
+          throw "Something went wrong"
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
   render() {
 
     const navigation = this.props.navigation;
@@ -131,9 +134,9 @@ export default class UpdatingUserProfile extends Component {
             />
           </View>
           <View style={styles.ButtonContainer}>
-          <TouchableOpacity style={styles.Button} onPress={() => this.UpdatingUserProfile()}>
-            <Text style={styles.TextButton}>Update</Text>
-          </TouchableOpacity>
+            <TouchableOpacity style={styles.Button} onPress={() => this.UpdatingUserProfile()}>
+              <Text style={styles.TextButton}>Update</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </ScrollView>
@@ -162,7 +165,7 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
   InputContainer: {
-    width:"60%",
+    width: "60%",
     marginBottom: 20,
   },
   TextLabel: {
